05e264967657ab20480da0060a61fd4636a36b41
SwaroopSRP <140191636+SwaroopSRP@users.noreply.github.com>
2024-05-05 10:30:21 +0000

    Reached Week 2 | Intro To Rust. Tried string and mutability of vars.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut txt = String::from("Hello, World!");
+    println!("{}", txt);
+    txt.clear();
+    println!("{}", txt);
+    if txt == String::from("") {
+        println!("True");
+    }
 }
