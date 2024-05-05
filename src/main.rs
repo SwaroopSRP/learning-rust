eab76f194f1b1bcd23d423694a68d90f1e644910
SwaroopSRP <140191636+SwaroopSRP@users.noreply.github.com>
2024-05-05 11:16:20 +0000

    Tried condtions and conditional-assignment.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let mut txt = String::from("Hello, World!");
-    println!("{}", txt);
-    txt.clear();
-    println!("{}", txt);
-    if txt == String::from("") {
-        println!("True");
+    let height = 190;
+    let result = if height >= 190 {
+        "Tall" // No semi-colon since it is a value being assigned to result via condition
     }
+    else if height > 170 {
+        "Average"
+    }
+    else {
+        "Short"
+    }; // The semi-colon that comes after assigning
+    println!("{}", result);
 }
